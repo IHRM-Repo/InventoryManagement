af9e699b848cfed5feed2d60759d5bebff537cda
David Mburu <mburud550@gmail.com>
2024-04-23 12:03:03 +0300

    updated web.php routes

--- routes/web.php
+++ routes/web.php
@@ -21,48 +21,32 @@ use Inertia\Inertia;
 */
 
 
-// Route::group([], function () {
-//    Route::get('/', [AssetController::class, 'index'])->name('item.home');
-//    Route::post('/', [AssetController::class, 'store'])->name('item.store');
-//    Route::get('{id}/view', [AssetController::class, 'show'])->name('item.show');
-//    Route::get('{id}/edit', [AssetController::class, 'edit'])->name('item.edit');
-//    Route::patch('{id}', [AssetController::class, 'update'])->name('item.update');
-//    Route::delete('/delete/{id}', [AssetController::class, 'destroy'])->name('item.destroy');
-//}); 
-// Route::get('/reports', [ReportsController::class, 'index'])->name('report');
-// Route::get('/dead-stock', [ReportsController::class, 'depreciatedStock'])->name('dead-stock');
-// Route::get('/export/dead-stock', [ReportsController::class, 'exportDeadStock'])->name('exportDeadStock');
-
-// Route::post('/import-excel', [ExcelFileImportController::class, 'upload'])->name('excel.upload');
-// Route::resource('/dashboard', StoreItemController::class);
-
 Route::get('/', function () {
     return Inertia::render('Auth/Login');
 });
 
-Route::group([], function () {
-    Route::get('/register', [RegisteredUserController::class, 'create'])->name('register'); 
-    Route::post('/register', [RegisteredUserController::class, 'store'])->name('register.user');  
-});
+Route::get('/dashboard', [AssetController::class, 'index'])->name('dashboard');
+Route::post('/dashboard', [AssetController::class, 'store'])->name('item.store');
+Route::get('{id}/view', [AssetController::class, 'show'])->name('item.show');
+Route::get('{id}/edit', [AssetController::class, 'edit'])->name('item.edit');
+Route::patch('{id}', [AssetController::class, 'update'])->name('item.update');
+Route::delete('/delete/{id}', [AssetController::class, 'destroy'])->name('item.destroy');
 
 
 
-//Route::get('/', function () {
-  //  return Inertia::render('Welcome', [
-    //    'canLogin' => Route::has('login'),
-      //  'canRegister' => Route::has('register'),
-    //]);
-//  }); 
+
+Route::get('/reports', [ReportsController::class, 'index'])->name('report');
+Route::get('/dead-stock', [ReportsController::class, 'depreciatedStock'])->name('dead-stock');
+Route::get('/export/dead-stock', [ReportsController::class, 'exportDeadStock'])->name('exportDeadStock');
+
+Route::post('/import-excel', [ExcelFileImportController::class, 'upload'])->name('excel.upload');
+Route::resource('/dashboard', StoreItemController::class);
 
 
-// Route::get('/dashboard', function () {
-//     return Inertia::render('Dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
 
 require __DIR__.'/auth.php';
